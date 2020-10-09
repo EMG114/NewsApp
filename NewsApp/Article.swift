61f15e3465e650e3deae3f84e46650674059ae28
Erica Gutierrez <egutierrez411@gmail.com>
2020-10-09 15:14:24 -0400

    Created Article model

---
+++ NewsApp/Article.swift
@@ -0,0 +1,21 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Erica on 10/9/20.
+//
+
+
+import UIKit
+
+struct Article {
+   // let source: Source
+    let author: String?
+    var title: String
+    var articleDescription: String?
+    var url: String
+    var urlToImage: String?
+    var publishedAt: Date
+    var content: String?
+    
+}
